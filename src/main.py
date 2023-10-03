c1b1237aeed6bb2b8411e3bf2f3fd88f08f9826d
Aaron-Uriel <therialaaronuriel@gmail.com>
2023-10-02 18:53:56 -0600

    Agregar lectura de datos. En este commit agrego lo básico de la lectura de datos, la creación de función de transferencia junto con la obtención de polos y ceros (eso todavía sigue roto).

--- src/main.py
+++ src/main.py
@@ -1,1 +1,28 @@
-print("Hello world")
+import scipy as sci
+import numpy as np
+import numpy.polynomial.polynomial as poly
+import control as ct
+
+
+
+def main():
+    num = input("Coeficientes del polinomio numerador: ").split();
+    den = input("Coeficientes del polinomio denominador: ").split();
+    num = list(map(float, num));
+    den = list(map(float, den));
+
+    zeros = poly.polyroots(poly.Polynomial(num))
+    poles = poly.polyroots(poly.Polynomial(num))
+
+    if (len(den) < len(num)):
+        print("Función de transferencia inválida");
+
+    # Función de transferencia.
+    trf_fn = ct.tf(num, den)
+
+    print("Los ceros de la función están en: ", zeros, "\n",
+          "Los polos de la función están en: ", poles, "\n")
+
+
+if __name__ == "__main__":
+    main()
