35e54524f0ea509e4d8c691921b10086dd94ed01
Aaron-Uriel <therialaaronuriel@gmail.com>
2023-10-03 15:19:32 -0600

    Añadir primera gráfica. La gráfica le hace falta un poco de retoques, muestra correctamente los datos.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
-import scipy as sci
 import numpy as np
 import numpy.polynomial.polynomial as poly
 import control as ct
-
-
+import matplotlib.pyplot as plt
 
 def main():
     num = input("Coeficientes del polinomio numerador: ").split();
@@ -11,8 +9,8 @@ def main():
     num = list(map(float, num));
     den = list(map(float, den));
 
-    zeros = poly.polyroots(poly.Polynomial(num))
-    poles = poly.polyroots(poly.Polynomial(num))
+    zeros = poly.polyroots(num)
+    poles = poly.polyroots(den)
 
     if (len(den) < len(num)):
         print("Función de transferencia inválida");
@@ -20,8 +18,20 @@ def main():
     # Función de transferencia.
     trf_fn = ct.tf(num, den)
 
-    print("Los ceros de la función están en: ", zeros, "\n",
-          "Los polos de la función están en: ", poles, "\n")
+    print("Los ceros de la función están en: ", zeros)
+    print("Los polos de la función están en: ", poles)
+
+    plt.style.use('_mpl-gallery')
+
+    fig, ax = plt.subplots(figsize=(5, 5), layout='constrained')
+    ax.scatter(zeros.real, zeros.imag, marker='x', label='Ceros')
+    ax.scatter(poles.real, poles.imag, marker='o', label='Polos')
+    ax.set_title('Polos y ceros')
+    ax.set_xlabel('$Re(z)$')
+    ax.set_ylabel('$Im(z)$')
+    ax.legend()
+
+    plt.show()
 
 
 if __name__ == "__main__":
