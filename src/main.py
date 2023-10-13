c606827b6091a822bcf9e4cdaf18fd05fd8e9e97
Aaron-Uriel <therialaaronuriel@gmail.com>
2023-10-13 09:46:32 -0600

    Pequeñas mejoras.

--- src/main.py
+++ src/main.py
@@ -2,40 +2,48 @@ import numpy as np
 import numpy.polynomial.polynomial as poly
 import control as ct
 import matplotlib.pyplot as plt
+from enum import Enum
+
+class Stability(Enum):
+    STABLE = 0
+    CRITICALLY_STABLE = 1
+    UNSTABLE = 2
+
 
 def main():
-    num = input("Coeficientes del polinomio numerador: ").split();
-    den = input("Coeficientes del polinomio denominador: ").split();
-    num = list(map(float, num));
-    den = list(map(float, den));
+    num = input("Coeficientes del polinomio numerador: ");
+    den = input("Coeficientes del polinomio denominador: ");
+    num = list(map(float, num.split()));
+    den = list(map(float, den.split()));
 
-    zeros = poly.polyroots(num)
-    poles = poly.polyroots(den)
+    # Función de transferencia.
+    sys_tf = ct.tf(num[::-1], den[::-1])
+    sys_ss = ct.tf2ss(num[::-1], den[::-1])
 
-    estado = "estable"
-    for i in poles:
-        if i.real == 0:
-            estado = "críticamente estable"
-        elif i.real > 0:
-            estado = "inestable"
-            break
+    zeros = sys_ss.zeros()
+    poles = sys_ss.poles()
 
     if (len(poles) < len(zeros)):
         print("Función de transferencia inválida");
         return 1
 
-    # Función de transferencia.
-    trf = ct.tf(num[::-1], den[::-1])
-    (t, y) = ct.step_response(trf)
-    print("Función de transferencia recibida:")
-    print(trf)
-    print("El orden de la función de transferencia es:", len(poles))
-    print("Los ceros de la función están en: ", zeros)
-    print("Los polos de la función están en: ", poles)
-    print("El estado del sistema es:", estado)
+    stability_status = Stability.STABLE
+    for i in poles:
+        if i.real == 0:
+            stability_status = Stability.CRITICALLY_STABLE
+        elif i.real > 0:
+            stability_status = Stability.UNSTABLE
+            break
 
-    #plt.style.use('_mpl-gallery')
-    plt.style.use('grayscale')
+    print("Información sobre la función de transferencia.")
+    print("Función de transferencia:", sys_tf)
+    print("Ceros:", zeros)
+    print("Polos: ", poles)
+    print("Orden del sistema:", len(poles))
+    print("Estabilidad del sistema:", stability_status.name)
+
+    plt.style.use('_mpl-gallery')
+    #plt.style.use('grayscale')
     fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2,
                                    figsize=(10, 5), layout='compressed')
     ax1.scatter(zeros.real, zeros.imag, marker='o', label='Ceros')
@@ -43,14 +51,16 @@ def main():
     ax1.set_title('Polos y ceros')
     ax1.set_xlabel('Componente real')
     ax1.set_ylabel('Componente imaginario')
-    ax1.grid()
+    ax1.grid(visible=True)
     ax1.legend()
 
-    ax2.plot(t, y)
+    response = ct.step_response(sys_tf)
+    ax2.plot(response.time, response.outputs, label='$y(t)$')
     ax2.set_title('Respuesta escalón unitario')
     ax2.set_ylabel('Magnitud')
     ax2.set_xlabel('Tiempo')
-    ax2.grid()
+    ax2.grid(visible=True)
+    ax2.legend()
 
     plt.show()
 
